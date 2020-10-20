dc68b3dbf8451edfe7a70934fcd8e694cf818eb6
arifahmadalfian <arifahmadalfian@gmail.com>
2020-10-20 16:27:38 +0700

    ConsumerApp

--- consumerapp/src/main/java/com/arifahmadalfian/consumerapp/MainActivity.kt
+++ consumerapp/src/main/java/com/arifahmadalfian/consumerapp/MainActivity.kt
@@ -1,12 +1,135 @@
 package com.arifahmadalfian.consumerapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.database.ContentObserver
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.PersistableBundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.arifahmadalfian.consumerapp.adapter.NoteAdapter
+import com.arifahmadalfian.consumerapp.db.DatabaseContract.NoteColumns.Companion.CONTENT_URI
+import com.arifahmadalfian.consumerapp.entity.Note
+import com.arifahmadalfian.consumerapp.helper.MappingHelper
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val EXTRA_STATE = "extra_state"
+    }
+
+    private lateinit var adapter: NoteAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = "Consumer Notes"
+
+        rv_notes.layoutManager = LinearLayoutManager(this)
+        rv_notes.setHasFixedSize(true)
+        adapter = NoteAdapter(this)
+        rv_notes.adapter = adapter
+
+        fab_add.setOnClickListener{
+            val intent = Intent(this@MainActivity, NoteAddUpdateActivity::class.java)
+            startActivityForResult(intent, NoteAddUpdateActivity.REQUEST_ADD)
+        }
+
+        val handlerThread = HandlerThread("DataObserver")
+        handlerThread.start()
+        val handler = Handler(handlerThread.looper)
+        val myObserver = object : ContentObserver(handler) {
+            override fun onChange(self: Boolean) {
+                loadNotesAsync()
+            }
+        }
+
+        contentResolver.registerContentObserver(CONTENT_URI, true, myObserver)
+
+        if (savedInstanceState == null) {
+            //proses ambil data
+            loadNotesAsync()
+        } else {
+            val list = savedInstanceState.getParcelableArrayList<Note>(EXTRA_STATE)
+            if (list != null) {
+                adapter.listNotes = list
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putParcelableArrayList(EXTRA_STATE, adapter.listNotes)
+    }
+
+    private fun loadNotesAsync() {
+        GlobalScope.launch(Dispatchers.Main) {
+            progressbar.visibility = View.VISIBLE
+            val defferdNotes = async(Dispatchers.IO) {
+                //val cursor = noteHelper.queryALL()
+                val cursor = contentResolver?.query(CONTENT_URI, null, null, null, null)
+                MappingHelper.mapCursorToArrayList(cursor)
+            }
+            progressbar.visibility = View.INVISIBLE
+            val notes = defferdNotes.await()
+            if (notes.size > 0) {
+                adapter.listNotes = notes
+            } else {
+                adapter.listNotes = ArrayList()
+                showSnackbarMessage("Tidak Ada Data Saat Ini")
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (data != null) {
+            when (requestCode) {
+                NoteAddUpdateActivity.REQUEST_ADD -> if (resultCode == NoteAddUpdateActivity.RESULT_ADD) {
+                    val note = data.getParcelableExtra<Note>(NoteAddUpdateActivity.EXTRA_NOTE)
+
+                    adapter.addItem(note)
+                    rv_notes.smoothScrollToPosition(adapter.itemCount - 1)
+
+                    showSnackbarMessage("Satu item berhasil ditambahkan")
+                }
+                NoteAddUpdateActivity.REQUEST_UPDATE ->
+                    when(resultCode) {
+                        NoteAddUpdateActivity.RESULT_UPDATE -> {
+                            val note = data.getParcelableExtra<Note>(NoteAddUpdateActivity.EXTRA_NOTE)
+                            val position = data.getIntExtra(NoteAddUpdateActivity.EXTRA_POSITION, 0)
+
+                            adapter.updateItem(position, note)
+                            rv_notes.smoothScrollToPosition(position)
+
+                            showSnackbarMessage("Satu item berhasil diubah")
+                        }
+                        NoteAddUpdateActivity.RESULT_DELETE -> {
+                            val position = data.getIntExtra(NoteAddUpdateActivity.EXTRA_POSITION, 0)
+                            adapter.removeItem(position)
+
+                            showSnackbarMessage("Satu item berhasil dihapus")
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun showSnackbarMessage(message: String) {
+        Snackbar.make(rv_notes, message, Snackbar.LENGTH_SHORT).show()
     }
 }
---
+++ consumerapp/src/main/java/com/arifahmadalfian/consumerapp/NoteAddUpdateActivity.kt
@@ -0,0 +1,185 @@
+package com.arifahmadalfian.consumerapp
+
+import android.content.ContentValues
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.arifahmadalfian.consumerapp.db.DatabaseContract
+import com.arifahmadalfian.consumerapp.db.DatabaseContract.NoteColumns.Companion.CONTENT_URI
+import com.arifahmadalfian.consumerapp.db.DatabaseContract.NoteColumns.Companion.DATE
+import com.arifahmadalfian.consumerapp.entity.Note
+import com.arifahmadalfian.consumerapp.helper.MappingHelper
+import kotlinx.android.synthetic.main.activity_note_add_update.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class NoteAddUpdateActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object {
+        const val EXTRA_NOTE = "extra_note"
+        const val EXTRA_POSITION = "extra_position"
+        const val REQUEST_ADD = 100
+        const val RESULT_ADD = 101
+        const val REQUEST_UPDATE =  200
+        const val RESULT_UPDATE = 201
+        const val RESULT_DELETE = 301
+        const val ALERT_DIALOG_CLOSE = 10
+        const val ALERT_DIALOG_DELETE = 20
+    }
+
+    private var isEdit = false
+    private var note: Note? = null
+    private var position: Int = 0
+    private lateinit var uriWithId: Uri
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_add_update)
+
+        note = intent.getParcelableExtra(EXTRA_NOTE)
+        if (note != null) {
+            position = intent.getIntExtra(EXTRA_POSITION, 0)
+            isEdit = true
+        } else {
+            note = Note()
+        }
+
+        val actionBarTitle: String
+        val btnTitle: String
+
+        if (isEdit) {
+
+            // Uri yang di dapatkan disini akan digunakan untuk ambil data dari provider
+            // content://com.arifahmadalfian.mynotesapp/note/id
+            uriWithId = Uri.parse(CONTENT_URI.toString() + "/" + note?.id)
+
+            val cursor = contentResolver.query(uriWithId, null, null, null, null)
+            if (cursor != null) {
+                note = MappingHelper.mapCursorToObject(cursor)
+                cursor.close()
+            }
+            actionBarTitle = "Ubah"
+            btnTitle = "Update"
+
+            note?.let {
+                et_title.setText(it.title)
+                et_description.setText(it.description)
+            }
+        } else {
+            actionBarTitle = "Tambah"
+            btnTitle = "Simpan"
+        }
+
+        supportActionBar?.title = actionBarTitle
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        btn_submit.text = btnTitle
+
+        btn_submit.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.btn_submit) {
+            val title = et_title.text.toString().trim()
+            val description = et_description.text.toString().trim()
+
+            if (title.isEmpty()) {
+                et_title.error = "Field can not be blank"
+                return
+            }
+
+            note?.title = title
+            note?.description = description
+
+            val intent = Intent()
+            intent.putExtra(EXTRA_NOTE, note)
+            intent.putExtra(EXTRA_POSITION, position)
+
+            val values = ContentValues()
+            values.put(DatabaseContract.NoteColumns.TITLE, title)
+            values.put(DatabaseContract.NoteColumns.DESCRIPTION, description)
+
+            if (isEdit) {
+                // Gunakan uriwithId untuk update
+                // content: //com.arifahmadalfian.mynotesapp/note
+                contentResolver.update(uriWithId, values, null, null)
+                Toast.makeText(this@NoteAddUpdateActivity, "Satu Item berhasil diedit", Toast.LENGTH_SHORT).show()
+                finish()
+            } else {
+                //note?.date = getCurrentDate()
+                values.put(DATE, getCurrentDate())
+                // Gunakan content uri untuk insert
+                //content://com.arifahmadalfian.mynotessapp/note/
+                contentResolver.insert(CONTENT_URI, values)
+                Toast.makeText(this@NoteAddUpdateActivity, "Satu Item berhasil disimpan", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private fun getCurrentDate(): String {
+        val dateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.getDefault())
+        val date = Date()
+
+        return dateFormat.format(date)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        if (isEdit) {
+            menuInflater.inflate(R.menu.menu_form, menu)
+        }
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_delete -> showAlertDialog(ALERT_DIALOG_DELETE)
+            android.R.id.home -> showAlertDialog(ALERT_DIALOG_CLOSE)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        showAlertDialog(ALERT_DIALOG_CLOSE)
+    }
+
+    private fun showAlertDialog(type: Int) {
+        val isDialogClose = type == ALERT_DIALOG_CLOSE
+        val dialogTitle: String
+        val dialogMessage: String
+
+        if (isDialogClose) {
+            dialogTitle = "Batal"
+            dialogMessage = "Apakah anda ingin membatalkan perubahan pada Form?"
+        } else {
+            dialogMessage = "Apakah andan yakin ingin menghapus item ini?"
+            dialogTitle = "Hapus Note"
+        }
+
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle(dialogTitle)
+        alertDialogBuilder
+            .setMessage(dialogMessage)
+            .setCancelable(false)
+            .setPositiveButton("Ya") { dialog, id ->
+                if (isDialogClose) {
+                    finish()
+                } else {
+                    // gunakan uriwithid dari intent activiti ini
+                    //content://com.arifahmadalfian.mynotesapp/note/id
+                    contentResolver.delete(uriWithId, null, null)
+                    Toast.makeText(this@NoteAddUpdateActivity, "Satu Item berhasil dihapus", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+            .setNegativeButton("Tidak") { dialog, id -> dialog.cancel() }
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+    }
+}
